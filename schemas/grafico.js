afd5ad96358f2e6cb01316061944b12e09d4ee1b
ArthurDalpiazTessele <arthurdtessele@gmail.com>
2023-11-16 19:29:20 -0300

    Ajuste-Graficos

--- schemas/grafico.js
+++ schemas/grafico.js
@@ -12,14 +12,29 @@ export default ({
             title: 'Gráfico',
         },
         {
+            name: 'descricao',
+            type: 'string',
+            title: 'Descrição',
+        },
+        {
             name: 'grafico2',
             type: 'image',
             title: 'Gráfico',
         },
         {
+            name: 'descricao2',
+            type: 'string',
+            title: 'Descrição 2',
+        },
+        {
             name: 'grafico3',
             type: 'image',
             title: 'Gráfico',
+        },
+        {
+            name: 'descricao3',
+            type: 'string',
+            title: 'Descrição ',
         },
     ]
 })
