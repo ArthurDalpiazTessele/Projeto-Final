f1d91e0fd5c96869c742eb42da120258313e50aa
ArthurDalpiazTessele <arthurdtessele@gmail.com>
2023-11-16 14:55:16 -0300

    Atual-professor

--- Página do Site/assets/script/membrodestaque.js
+++ Página do Site/assets/script/membrodestaque.js
@@ -1,6 +1,6 @@
 
 async function dadoAsync(){
-    var resposta = await fetch("https://pecgm0gy.api.sanity.io/v2021-10-21/data/query/production?query=*%5B_type+%3D%3D+%22membrosDestaque%22%5D%7B%0A++%22imagem%22%3Aimagem.asset-%3Eurl%2C%0A++%22nome%22%3Anome%2C%0A++%22sobrenome%22%3Asobrenome%2C%0A++%22cargo%22%3Acargo%2C%0A%0A%7D",{
+    var resposta = await fetch("https://vksmkbdz.api.sanity.io/v2022-03-07/data/query/production?query=*%5B_type+%3D%3D+%22professor%22%5D%7B%0A+++%22nome%22%3A+nome%2C%0A+++%22cargo%22%3Acargo%2C%0A+++%22bio%22%3Abio%2C%0A+++%22imagem%22%3Aimagem%2C%0A+%7D",{
         method: "GET"
     });
     var minharesposta = await resposta.json();
@@ -14,6 +14,6 @@ async function dadoAsync(){
     var h4 = document.querySelector("h4.h4destaque")
     h4.innerText = `${minharesposta.result[0].cargo}`
     var texto = document.createElement("p");
-    texto.inner
+    texto.innerText()
 }
 dadoAsync();
