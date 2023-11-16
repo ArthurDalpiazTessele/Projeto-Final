dd49f71aa0a67501b69ab3f1d8305ce0f654b01e
ArthurDalpiazTessele <arthurdtessele@gmail.com>
2023-11-16 15:01:10 -0300

    Atual-Pilares

--- Página do Site/assets/script/pilares.js
+++ Página do Site/assets/script/pilares.js
@@ -1,6 +1,6 @@
 
 async function dadoAsync(){
-    var resposta = await fetch("https://pecgm0gy.api.sanity.io/v2021-10-21/data/query/production?query=*%5B_type+%3D%3D+%22pilares%22%5D%7B%0A++%22descricao%22%3Adescricao%2C%0A++%22listaPilar%22%3AlistaPilar%2C%0A%7D",{
+    var resposta = await fetch("https://vksmkbdz.api.sanity.io/v2022-03-07/data/query/production?query=*%5B_type+%3D%3D+%22pilares%22%5D%7B%0A+++%22pilar%22%3A+pilar%2C%0A+%7D",{
         method: "GET"
     });
     var minharesposta = await resposta.json();
