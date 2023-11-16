32d4cdd55b1f362a960a2e4ee4b0695c7f056608
ArthurDalpiazTessele <arthurdtessele@gmail.com>
2023-11-16 15:05:42 -0300

    Atual-QrCode

--- Página do Site/assets/script/qrcode.js
+++ Página do Site/assets/script/qrcode.js
@@ -1,6 +1,6 @@
 
 async function dadoAsync(){
-    var resposta = await fetch("https://pecgm0gy.api.sanity.io/v2021-10-21/data/query/production?query=*%5B_type+%3D%3D+%22qrcode%22%5D%7B%0A++%22qrcode%22%3Aqrcode.asset-%3Eurl%2C%0A++++%0A%7D",{
+    var resposta = await fetch("https://vksmkbdz.api.sanity.io/v2022-03-07/data/query/production?query=*%5B_type+%3D%3D+%22qrCode%22%5D%7B%0A+++%22qrCode%22%3A+qrCode%2C%0A+%7D",{
         method: "GET"
     });
     var minharesposta = await resposta.json();
